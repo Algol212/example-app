be2c1e8a71a925466db35310af5ee40d7c276829
Algol212 <algol212339@gmail.com>
2023-02-25 01:53:46 +0900

    ログイン機能に関数修正

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -9,8 +9,9 @@
     <title>つぶやきアプリ</title>
 </head>
 <body>
-   <h1>つぶやきアプリ</h1>
-   <div>
+    <h1>つぶやきアプリ</h1>
+    @auth
+    <div>
         <p>投稿フォーム</p>
         @if (session('feedback.success'))
             <p style="color: green">{{ session('feedback.success') }}</p>
@@ -25,21 +26,27 @@
             @enderror
             <button type="submit">投稿</button>
         </form>
-   </div>
-   <div>
+    </div>
+    @endauth
+    <div>
     @foreach ($tweets as $tweet)
         <details>
-            <summary>{{ $tweet->content }}</summary>
-            <div>
-                <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit">削除</button>
-                </form>
-            </div>
+            <summary>{{ $tweet->content }} by {{ $tweet->user->name }}
+            </summary>
+            @if(\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
+                <div>
+                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit">削除</button>
+                    </form>
+                </div>
+            @else
+                編集できません
+            @endif
         </details>
     @endforeach
-   </div>
+    </div>
 </body>
 </html>
